0d205a9cc0f9e19dfa74f179ab906cd27c902360
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-08-13 23:50:09 -0300

    feat: Home - Skeleton

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,12 +1,15 @@
 import { Banners } from "@/components/home/banners";
 import { InfoArea } from "@/components/home/info-area";
+import { ProductListSkeleton } from "@/components/home/product-list-skeleton";
 import { data } from "@/data";
 
 export default function Page() {
   return (
-    <div>
+    <div className="pb-96">
       <Banners list={data.banners} />
       <InfoArea />
+      <ProductListSkeleton />
+      <ProductListSkeleton />
     </div>
   );
 }
