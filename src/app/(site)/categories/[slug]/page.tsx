bb56e82ac6e111084a9819098a42fb342a4f0d9e
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-08-15 23:32:46 -0300

    feat: Categorias - Filtros 2

--- src/app/(site)/categories/[slug]/page.tsx
+++ src/app/(site)/categories/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { ProductListFilter } from "@/components/categories/product-list-filter";
+
 type Props = {
   params: Promise<{ slug: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
@@ -16,29 +18,7 @@ export default async function Page({ params, searchParams }: Props) {
         <Link href={""}>Categoria</Link> &gt; Temporário
       </div>
 
-      <div className="flex flex-col md:flex-row gap-6 justify-between items-start md:items-center">
-        <div className="text-3xl">
-          <strong>99</strong> Produtos
-        </div>
-        <div className="flex flex-row w-full md:max-w-70 gap-5">
-          <select className="flex-1 flex items-center px-6 h-14 bg-white border border-gray-200 rounded-sm text-gray-500">
-            <option value="">Ordenar por</option>
-          </select>
-          <div className="flex-1 md:hidden flex items-center px-6 h-14 bg-white border border-gray-200 rounded-sm text-gray-500">
-            Filtrar por
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col md:flex-row gap-8 mt-8">
-        <div className="flex-1 md:max-w-70">Filtro</div>
-        <div className="flex-1 grid grid-cols-1 md:grid-cols-3">
-          <div>Produto</div>
-          <div>Produto</div>
-          <div>Produto</div>
-          <div>Produto</div>
-        </div>
-      </div>
+      <ProductListFilter />
     </div>
   );
 }
