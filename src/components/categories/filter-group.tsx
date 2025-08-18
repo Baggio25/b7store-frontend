4421fb5fdf7e614e1602526d1cea943e8af8b4eb
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-08-17 22:57:50 -0300

    feat: Categorias - Filtros 4

--- src/components/categories/filter-group.tsx
+++ src/components/categories/filter-group.tsx
@@ -1,13 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
 import { FilterItem } from "./filter-item";
 
 export function FilterGroup() {
+  const [opened, setOpened] = useState(true);
+
   return (
     <div className="mb-8">
-      <div className="flex justify-between items-center border-b border-gray-200">
-        <div className="flex-1 font-bold text-xl pb-4">Nome do grupo</div>
-        <div className="">...</div>
+      <div className="flex justify-between items-center border-b border-gray-200 pb-4">
+        <div className="flex-1 font-bold text-xl">Nome do grupo</div>
+        <div
+          onClick={() => setOpened(!opened)}
+          className="size-8 cursor-pointer flex justify-center items-center"
+        >
+          <Image
+            src={"/assets/ui/arrow-left-s-line.png"}
+            alt=""
+            width={24}
+            height={24}
+            className={`${opened ? "rotate-0" : "rotate-180"} transition-all`}
+          />
+        </div>
       </div>
-      <div className="pt-8 flex flex-col gap-4">
+      <div
+        className={`overflow-y-hidden ${
+          opened ? "max-h-96" : "max-h-0"
+        } transition-all`}
+      >
         <FilterItem id={1} label={"item1"} />
         <FilterItem id={2} label={"item2"} />
         <FilterItem id={3} label={"item3"} />
